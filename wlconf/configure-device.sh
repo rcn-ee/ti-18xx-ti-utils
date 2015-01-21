42c49d2bac2dcac3b45beab9971e6763419377a9
Guy Mishol <guym@ti.com>
2015-01-21 16:46:07 +0200

    wlconf: update configure-device script
    easy configuration script for the
    WLAN device according to user inputs
    
    Signed-off-by: Guy Mishol <guym@ti.com>

--- wlconf/configure-device.sh
+++ wlconf/configure-device.sh
@@ -1,147 +1,228 @@
 #!/bin/sh
-# Script to run on the target to configure the wl18xx-conf.bin file to match the device capabilities and 
-# antennae configuration. Takes as an argument the device name - wl1801,wl1805, wl1831, wl1835
+# Script to run on the target to configure the wl18xx-conf.bin file to match the device capabilities
 
+# version
+VERSION=1.0
 
+# defaults
 binary_name="/lib/firmware/ti-connectivity/wl18xx-conf.bin"
+
+
+# function for printing help
+print_help()
+{
+	echo "Usage:  configure-device.sh <option>"
+	echo "Options:"
+	echo "      --version       show version"
+	echo "      --help          show help"
+	echo "      --dump			save wl18xx-conf.bin dump to wl18xx-conf-dump.txt"
+	echo "      <empty>			configuration mode"
+}
+
+
+# function for printing dump
+print_dump()
+{
+	echo "Saving wl18xx-conf.bin dump to wl18xx-conf-dump.txt"
+	./wlconf -i $binary_name --get > wl18xx-conf-dump.txt
+}
+
+# function for printing summary
+print_summary()
+{
+	echo ""
+	echo "The device has been successfully configured."
+	echo "TI Module: "$ti_mod
+	echo "Chip Flavor: "$CHIP_FLAVOR
+	echo "Number of 2.4GHz antennas fitted: "$number_2_4G_antenna
+	echo "Number of 5GHz antennas fitted: "$number_5G_antenna
+	echo "Diversity Support: "$diversity_5g
+	echo "SISO40 Support: "$siso40mhz
+	echo "Japanese standards applied: "$japan
+	echo ""
+}
+
+
+# check for user option
+if [ ! -z "$1" ]; then
+	case $1 in
+		"--version") echo "Version" $VERSION;exit;;
+		"--help") print_help;exit;;
+		"--dump") print_dump;exit;;
+		*) echo "Unknown option entered";print_help;exit;;
+	esac
+fi
+
+# start configuration
+echo ""
+echo "Please provide the following information."
+echo ""
+
+# ask if TI module is in use
+TI_MODULE=-1;
+while [ $TI_MODULE -eq -1 ]
+do
+	read -p 'Are you using a TI module? [y/n] : ' ti_mod
+	case $ti_mod in
+	    "n") TI_MODULE=0;;
+	    "N") TI_MODULE=0;;
+	    "y") TI_MODULE=1;;
+	    "Y") TI_MODULE=1;;
+	    *) echo "Please enter y or n.";TI_MODULE=-1;continue;;
+    esac
+done
+
 
 # logic to be followed here is that if a device has a parameter with a value over 1 then there is a choice 
 # that has to be made by user as to whether the feature will be used.
-# eg a 2.4GHz MIMO capable device may only have a single antenna fitted
+# e.g. a 2.4GHz MIMO capable device may only have a single antenna fitted
 # assume that as 2.4GHz does not pass WFA test with 40MHz channels then we disable it for ALL devices
-if [ $# -eq 1 ]; then
-  case $1 in
-    wl1801) number_2_4G_antenna=1; number_5G_antenna=0;ht40=0
-      ;;
-    wl1805) number_2_4G_antenna=1; number_5G_antenna=0;ht40=0
-      ;;
-    wl1807) number_2_4G_antenna=2; number_5G_antenna=1;ht40=0
-      ;;
-    wl1831) number_2_4G_antenna=2; number_5G_antenna=0;ht40=0
-      ;;
-    wl1835) number_2_4G_antenna=2; number_5G_antenna=0;ht40=0
-      ;;
-    wl1837) number_2_4G_antenna=2; number_5G_antenna=1;ht40=0
-      ;;
-   *) echo "Unknown device";exit ;;
-  esac
+CHIP_FLAVOR=-1;
+while [ $CHIP_FLAVOR -eq -1 ]
+do
+	read -p 'What is the chip flavor? [1801/1805/1807/1831/1835/1837 or 0 for unknown] : ' CHIP_FLAVOR
+	case $CHIP_FLAVOR in
+		1801) number_2_4G_antenna=1; number_5G_antenna=0;;
+		1805) number_2_4G_antenna=2; number_5G_antenna=0;;
+		1807) number_2_4G_antenna=2; number_5G_antenna=1;;
+		1831) number_2_4G_antenna=1; number_5G_antenna=0;;
+		1835) number_2_4G_antenna=2; number_5G_antenna=0;;
+		1837) number_2_4G_antenna=2; number_5G_antenna=1;;
+		0) number_2_4G_antenna=2; number_5G_antenna=1;;
+		*) echo "Please enter 1801/1805/1807/1831/1835/1837 or 0 for unknown.";CHIP_FLAVOR=-1;continue;;
+	esac
+done
+
+
+# decide upon the correct ini file
+JP_STANDARD=-1;
+japan="n";
+if [ $TI_MODULE -eq 0 ]; then
+	ini_file_name="official_inis/WL8_COB_INI.ini"
+else
+	case $CHIP_FLAVOR in
+			1801) ini_file_name="official_inis/WL1835MOD_INI.ini"; jp_support=0;;
+			1805) ini_file_name="official_inis/WL1835MOD_INI.ini"; jp_support=0;;
+			1807) ini_file_name="official_inis/WL1837MOD_INI_FCC_CE.ini"; jp_support=1;;
+			1831) ini_file_name="official_inis/WL1835MOD_INI.ini"; jp_support=0;;
+			1835) ini_file_name="official_inis/WL1835MOD_INI.ini"; jp_support=0;;
+			1837) ini_file_name="official_inis/WL1837MOD_INI_FCC_CE.ini"; jp_support=1;;
+			0) ini_file_name="official_inis/WL1835MOD_INI.ini"; jp_support=0;;
+    esac
+	
+	while [ $JP_STANDARD -eq -1 ]
+	do
+		if [ $jp_support -eq 1 ]; then
+			read -p 'Should Japanese standards be applied? [y/n] : ' japan
+			case $japan in
+				"n") ini_file_name="official_inis/WL1837MOD_INI_FCC_CE.ini";JP_STANDARD=0;;
+				"N") ini_file_name="official_inis/WL1837MOD_INI_FCC_CE.ini";JP_STANDARD=0;;
+				"y") ini_file_name="official_inis/WL1837MOD_INI_FCC_CE_JP.ini";JP_STANDARD=0;;  			 
+				"Y") ini_file_name="official_inis/WL1837MOD_INI_FCC_CE_JP.ini";JP_STANDARD=0;;
+				*) echo "Please enter y or n";JP_STANDARD=-1;continue;;
+			esac
+		else
+			# set to required number to exit loop
+			JP_STANDARD=$jp_support;
+		fi
+	done
 fi
 
-# For all MODULE devices low band component needs to be set to 0x2
-low_band_component=0x2
+# check if the correct ini file exist
+if [ ! -e $ini_file_name ]; then                                                                                                                     
+    echo "$ini_file_name does not exist. Please contact your local TI representative or post a question on http://e2e.ti.com"
+	exit;  
+fi
 
-# and board_type is 4
-board_type=4
 
-# ask if one or two antennae will be fitted in 2.4GHz band
-NUM_ANTENNAE=-1;
-while [ $NUM_ANTENNAE -eq -1 ]
+# ask if one or two antennas will be fitted in 2.4GHz band
+NUM_OF_ANTENNAS=-1;
+while [ $NUM_OF_ANTENNAS -eq -1 ]
 do
-  if [ $number_2_4G_antenna -gt 1 ]; then
-        echo "Device is capable of supporting 2 antenna in 2.4Ghz band"
-	read -p 'how many antennae are fitted? [1/2] : ' NUM_ANTENNAE
-	case $NUM_ANTENNAE in
-	    "1") number_2_4G_antenna=1;;
-	    "2") number_2_4G_antenna=2;;
-	    *)  echo "Please enter 1 or 2";NUM_ANTENNAE=-1;continue;;
+	if [ $number_2_4G_antenna -gt 1 ]; then
+		read -p 'How many 2.4GHz antennas are fitted? [1/2] : ' NUM_OF_ANTENNAS
+		case $NUM_OF_ANTENNAS in
+			"1") number_2_4G_antenna=1;;
+			"2") number_2_4G_antenna=2;;
+			*) echo "Please enter 1 or 2 antennas";NUM_OF_ANTENNAS=-1;continue;;
         esac
-  else
-     # set to required number to exit loop
-     NUM_ANTENNAE=$number_2_4G_antenna;
-  fi
+	else
+		# set to required number to exit loop
+		NUM_OF_ANTENNAS=$number_2_4G_antenna;
+	fi
 done
+
 
 # ask if a 5GHz antenna will be fitted
-NUM_ANTENNAE=-1;
-while [ $NUM_ANTENNAE -eq -1 ]
+NUM_OF_ANTENNAS=-1;
+while [ $NUM_OF_ANTENNAS -eq -1 ]
 do
-  if [ $number_5G_antenna -eq 1 ]; then
-        echo "Device is capable of supporting 5GHz"
-	read -p 'how many 5GHz antennae are fitted? [0/1] : ' NUM_ANTENNAE
-	case $NUM_ANTENNAE in
-	    "0") number_5G_antenna=0;;
-	    "1") number_5G_antenna=1;;
-	    *)  echo "Please enter 0 or 1";NUM_ANTENNAE=-1;continue;;
+	if [ $number_5G_antenna -eq 1 ]; then
+		read -p 'How many 5GHz antennas are fitted? [0/1] : ' NUM_OF_ANTENNAS
+		case $NUM_OF_ANTENNAS in
+			"0") number_5G_antenna=0;;
+			"1") number_5G_antenna=1;;
+			*) echo "Please enter 0 or 1";NUM_OF_ANTENNAS=-1;continue;;
         esac
-  else
-     # set to required number to exit loop
-     NUM_ANTENNAE=$number_5G_antenna;
-
-  fi
+	else
+		# set to required number to exit loop
+		NUM_OF_ANTENNAS=$number_5G_antenna;
+	fi
 done
+
+
+# ask if 5GHz diversity is needed
+DIVERSITY=-1;
+diversity_5g="n";
+high_band_component_type=0x09;
+while [ $DIVERSITY -eq -1 ]
+do
+	if [ $TI_MODULE -eq 0 ] && [ $number_5G_antenna -eq 1 ]; then
+		read -p 'Should 5GHz diversity be applied? [y/n] : ' diversity_5g
+		case $diversity_5g in
+			"n") high_band_component_type=0x09;DIVERSITY=0;;
+			"N") high_band_component_type=0x09;DIVERSITY=0;;
+			"y") high_band_component_type=0x0a;DIVERSITY=0;;  			 
+			"Y") high_band_component_type=0x0a;DIVERSITY=0;;
+			*) echo "Please enter y or n";DIVERSITY=-1;continue;;
+        esac
+	else
+		# set to required number to exit loop
+		DIVERSITY=0;
+	fi
+done
+
 
 # ask if SISO40 should be enabled
-SISO=-1;
-while [ $SISO -eq -1 ]
+SISO40=-1;
+while [ $SISO40 -eq -1 ]
 do
- if [ $ht40 -eq 1 ]; then
-        echo "Device is capable of supporting SISO40"
-	read -p 'should it be enabled? [y/n] : ' SISO
-	case $SISO in
-	    "n") ht40=0;SISO=0;;
-	    "N") ht40=0;SISO=0;;
-	    "y") ht40=1;SISO=0;;
-	    "Y") ht40=1;SISO=0;;
-	    *)  echo "Please enter y or n";SISO=-1;continue;;
-        esac
- else
-    # set to required number to exit loop
-    SISO=$ht40;
- fi
+	read -p 'Should SISO40 support be applied? [y/n] : ' siso40mhz
+	case $siso40mhz in
+		"n") ht40=0;SISO40=0;;
+		"N") ht40=0;SISO40=0;;
+		"y") ht40=1;SISO40=0;;  			 
+		"Y") ht40=1;SISO40=0;;
+		*) echo "Please enter y or n";SISO40=-1;continue;;
+    esac
 done
 
-
-# Now check if a wl18xx-conf.bin file already exists and ask if a new one should be created from .ini
-# ensure we are in correct directory for wlconf
-MODIFY_CONF=0
-modify_conf_file=0
+# remove the wlcore_sdio module before updating the bin file
 rmmod wlcore_sdio
 
-cd /usr/sbin/wlconf
 
-if [ -e $binary_name ]; then
-        echo "A wl18xx-conf.bin file already exists, do you want to modify it (y) or generate a clean one from .ini (n)"
-	read -p 'modify it? [y/n] : ' MODIFY_CONF
-	case $MODIFY_CONF in
-	    "n") modify_conf_file=0;;
-	    "N") modify_conf_file=0;;
-	    "y") modify_conf_file=1;;
-	    "Y") modify_conf_file=1;;
-	    *)  echo "Please enter y or n: ";MODIFY_CONF=0;continue;;
-        esac
-fi
-
-ini_file_name="official_inis/WL1835MOD_INI.ini"
-INPUT_NAME=0
-
-if [ $modify_conf_file -eq 0 ]; then 
-   if [ -e $ini_file_name ]; then
-      echo "Create clean wl18xx-conf.bin from $ini_file_name"
-   else
-      echo "$ini_file_name does not exist, "
-      read -p 'enter another ini file name: ' INPUT_NAME
-      ini_file_name=$INPUT_NAME
-      if [ ! -e $ini_file_name ]; then
-        echo "$ini_file_name does not exist in /usr/sbin/wlconf"
-        exit 1
-      fi
-   fi
-   
-   # Now create clean file
-   ./wlconf -o $binary_name -I $ini_file_name
-fi   
-
-
-
-#finally write values to binary
+# create clean file
+./wlconf -o $binary_name -I $ini_file_name
+  
+  
+# finally write values to binary
 ./wlconf -i $binary_name -o $binary_name -s wl18xx.phy.number_of_assembled_ant2_4=$number_2_4G_antenna
 ./wlconf -i $binary_name -o $binary_name -s wl18xx.phy.number_of_assembled_ant5=$number_5G_antenna
+./wlconf -i $binary_name -o $binary_name -s wl18xx.phy.high_band_component_type=$high_band_component_type
 ./wlconf -i $binary_name -o $binary_name -s wl18xx.ht.mode=$ht40
-./wlconf -i $binary_name -o $binary_name -s wl18xx.phy.low_band_component=$low_band_component
-./wlconf -i $binary_name -o $binary_name -s wl18xx.phy.board_type=$board_type
 
+print_summary
 
-
-#reinsert ko
+# reinsert the wlcore_sdio
 modprobe wlcore_sdio
 
